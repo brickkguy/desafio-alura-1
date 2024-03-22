5137afc9045f2b6a75bb80c22df8a1e5d2c57906
brickkguy <pedro.ferreira.amorim.99@gmail.com>
2024-03-22 20:41:44 -0300

    commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,31 @@
+var textInput = document.querySelector("#input-texto");
+var outInput = document.querySelector("#output");
 
+
+function criptografar(){
+
+  var texto = textInput.value;
+
+  var resultadoCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
+
+  
+  document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultadoCripto + 
+  '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+}
+
+function descriptografar(){
+
+    var texto = textInput.value;
+
+    var resultadoDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+  
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">' + resultadoDescripto + 
+    '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar</button>'
+}
+
+function copiar() {
+    var textoCopiado = document.getElementById('input-texto');
+  
+    textoCopiado.select();
+    document.execCommand('copy');
+}  
