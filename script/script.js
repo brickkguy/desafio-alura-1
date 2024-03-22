65b5e9836291a3f303bf147f9829468d49616742
brickkguy <pedro.ferreira.amorim.99@gmail.com>
2024-03-22 12:40:27 -0300

    skibidi figma

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let mindsetNumber = prompt('Digite um número.');
+console.log(mindsetNumber);
+alert('Eu vou ler a sua mente.');
+alert('Se prepare.');
+let resposta = prompt(`O seu número era ${mindsetNumber}? (s/n)`);
+if (resposta == 's') {
+    alert('Tá tudo aqui no meu mindset.')
+} else {
+    alert('Se direcione às fezes.')
+}
